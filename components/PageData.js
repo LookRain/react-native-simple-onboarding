a1e0aa64248994c99d8afa1047e3fdc6ce9723e9
Lu Yu <brantscube@hotmail.com>
2019-06-25 13:58:00 +0800

    Update PageData.js

--- components/PageData.js
+++ components/PageData.js
@@ -36,11 +36,10 @@ const styles = {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
   image: {
     flex: 0,
-    paddingBottom: 60,
     alignItems: 'center',
   },
   title: {
